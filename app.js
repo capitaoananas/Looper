e7e3454976d047ec57e5d6db5a0cda3a9111c2c8
Lukas Reis <155737845+capitaoananas@users.noreply.github.com>
2025-05-05 13:24:27 +0100

    detect notes and velocity with midi keyboard

--- app.js
+++ app.js
@@ -3,7 +3,46 @@ if (navigator.requestMIDIAccess) {
 }
 
 function success(midiAccess) {
-  console.log(midiAccess);
+  midiAccess.onstatechange = updateDevices;
+
+  const inputs = midiAccess.inputs;
+
+  inputs.forEach((input) => {
+    input.addEventListener("midimessage", handleInput);
+  });
+}
+
+function handleInput(input) {
+  const command = input.data[0];
+  const note = input.data[1];
+  const velocity = input.data[2];
+
+  switch (command) {
+    case 145: // note on
+      if (velocity > 0) {
+        noteOn(note, velocity);
+      } else {
+        noteOff(note);
+      }
+      break;
+    case 129: // note off
+      noteOff(note);
+      break;
+  }
+}
+
+function noteOn(note, velocity) {
+  console.log(note, velocity);
+}
+
+function noteOff(note) {
+  console.log(note);
+}
+
+function updateDevices(event) {
+  console.log(
+    `Name: ${event.port.name}, Brand: ${event.port.manufacturer}, State: ${event.port.state}, Type: ${event.port.type}`,
+  );
 }
 
 function failure() {
